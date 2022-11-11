bc0ffab2c7209d8e3923154d01f511d83336ff9c
Ygor Goulart <ygorgm95@gmail.com>
2022-11-11 11:51:19 -0300

    feat: mobile responsive

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -11,13 +11,18 @@ export class NavbarComponent implements OnInit {
   isMobile: boolean = false;
   getIsMobile(): boolean {
     const w = document.documentElement.clientWidth;
-    const breakpoint = 992;
+    const breakpoint = 860;
     console.log(w);
     if (w < breakpoint) {
       return true;
     } else {
       return false;
     }
+  }
+
+  mobileMenu: boolean = false;
+  mobileMenuClick() {
+    this.mobileMenu = !this.mobileMenu;
   }
 
   ngOnInit(): void {
