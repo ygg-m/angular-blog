05eb099bc18bbb07e6d1149cd7bca90106ed114a
Ygor Goulart <ygorgm95@gmail.com>
2022-11-11 11:11:34 -0300

    feat: screen width tracker

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.sass']
+  styleUrls: ['./navbar.component.sass'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  isMobile: boolean = false;
+  getIsMobile(): boolean {
+    const w = document.documentElement.clientWidth;
+    const breakpoint = 992;
+    console.log(w);
+    if (w < breakpoint) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
+  ngOnInit(): void {
+    this.isMobile = this.getIsMobile();
+    window.onresize = () => {
+      this.isMobile = this.getIsMobile();
+    };
+  }
 }
