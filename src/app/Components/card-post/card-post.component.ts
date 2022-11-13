1d1ede68b0996402c051351655306b74b97c6a65
Ygor Goulart <ygorgm95@gmail.com>
2022-11-13 13:20:15 -0300

    style: add dinamic content

--- src/app/Components/card-post/card-post.component.ts
+++ src/app/Components/card-post/card-post.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-post',
   templateUrl: './card-post.component.html',
-  styleUrls: ['./card-post.component.sass']
+  styleUrls: ['./card-post.component.sass'],
 })
 export class CardPostComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() img_src: string = '../../../assets/placeholder_post-img.png';
+  @Input() img_alt: string = 'Post Image without description.';
+  @Input() author: string = 'Anonymous';
+  @Input() date: string = 'Today';
+  @Input() title: string = 'No title';
+  @Input() description: string = 'No description';
+  @Input() tags: string[] = [];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
