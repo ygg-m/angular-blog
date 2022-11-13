09e1d67c1bcbdb67104ccfad919e5c05c3cb7512
Ygor Goulart <ygorgm95@gmail.com>
2022-11-13 13:02:29 -0300

    feat: data binding in featured-post

--- src/app/Components/featured-post/featured-post.component.ts
+++ src/app/Components/featured-post/featured-post.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-featured-post',
   templateUrl: './featured-post.component.html',
-  styleUrls: ['./featured-post.component.sass']
+  styleUrls: ['./featured-post.component.sass'],
 })
 export class FeaturedPostComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() img_src: string = '../../../assets/placeholder_post-img.png';
+  @Input() img_alt: string = 'Post Image without description.';
+  @Input() author: string = 'Anonymous';
+  @Input() date: string = 'Today';
+  @Input() title: string = 'No title';
+  @Input() description: string = 'No description';
+  @Input() tags: string[] = [];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
