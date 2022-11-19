55f81716bfb34e94d0b2be8f2e42097d3a58f5dc
Ygor Goulart <ygorgm95@gmail.com>
2022-11-18 22:03:56 -0300

    remove categories component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardPostComponent } from './Components/card-post/card-post.component';
-import { CategoriesComponent } from './Components/categories/categories.component';
 import { FeaturedPostComponent } from './Components/featured-post/featured-post.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { NavigationComponent } from './Components/navigation/navigation.component';
 import { PageTitleComponent } from './Components/page-title/page-title.component';
 import { SubscribeComponent } from './Components/subscribe/subscribe.component';
-import { PostComponent } from './Pages/post/post.component';
 import { HomeComponent } from './Pages/home/home.component';
+import { PostComponent } from './Pages/post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     PageTitleComponent,
-    CategoriesComponent,
     FeaturedPostComponent,
     CardPostComponent,
     NavigationComponent,
