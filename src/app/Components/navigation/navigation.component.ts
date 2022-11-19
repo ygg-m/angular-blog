aa89f09491b1812ba8ff0c19b7dcf23d1391be0e
Ygor Goulart <ygorgm95@gmail.com>
2022-11-18 22:30:44 -0300

    feat: add click to previous and next buttons

--- src/app/Components/navigation/navigation.component.ts
+++ src/app/Components/navigation/navigation.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.sass']
+  styleUrls: ['./navigation.component.sass'],
 })
 export class NavigationComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  previousPage() {
+    alert("This button doesn't have a function yet! :(");
   }
-
+  nextPage() {
+    alert("This button doesn't have a function yet! :(");
+  }
 }
