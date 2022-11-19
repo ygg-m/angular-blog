751fa0a6f191e8aefc50854a6bea4ed34df34bd6
Ygor Goulart <ygorgm95@gmail.com>
2022-11-18 21:42:23 -0300

    fix: posts working

--- src/app/Components/featured-post/featured-post.component.ts
+++ src/app/Components/featured-post/featured-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { posts } from 'src/app/Data/Posts';
 
 @Component({
@@ -17,14 +18,22 @@ export class FeaturedPostComponent implements OnInit {
   @Input() post: boolean = false;
   @Input() id: string | number | null = 0;
 
-  constructor() {}
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    if (this.post)
+      this.route.paramMap.subscribe({
+        next: (params) => {
+          this.id = params.get('id');
+          this.setParamsToComponent(this.id);
+        },
+      });
+
     this.setParamsToComponent(this.id);
   }
 
-  setParamsToComponent(id: string | number | null) {
-    const result = posts.filter((article) => article.id === id)[0];
+  setParamsToComponent(id: string | number | null): void {
+    const result = posts.filter((article) => article.id.toString() == id)[0];
 
     this.img_src = result.img_src;
     this.img_alt = result.img_alt;
--- src/app/Pages/post/post.component.ts
+++ src/app/Pages/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { posts } from 'src/app/Data/Posts';
 
@@ -8,34 +8,38 @@ import { posts } from 'src/app/Data/Posts';
   styleUrls: ['./post.component.sass'],
 })
 export class PostComponent implements OnInit {
-  img_src: string = '';
-  img_alt: string = '';
-  author: string = '';
-  date: string = '';
-  title: string = '';
-  description: string = '';
-  tags: string[] = [];
-  content: string = '';
-  id: string | number | null = 0;
+  // img_src: string = '';
+  // img_alt: string = '';
+  // author: string = '';
+  // date: string = '';
+  // title: string = '';
+  // description: string = '';
+  // tags: string[] = [];
+  @Input() content: string = '';
+  @Input() id: string | number | null = 0;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     window.scrollTo(0, 0);
-    this.route.paramMap.subscribe((params) => (this.id = params.get('id')));
-    this.setParamsToComponent(this.id);
+    this.route.paramMap.subscribe({
+      next: (params) => {
+        this.id = params.get('id');
+        this.setParamsToComponent(this.id);
+      },
+    });
   }
 
   setParamsToComponent(id: string | number | null) {
     const result = posts.filter((article) => article.id.toString() === id)[0];
 
-    this.img_src = result.img_src;
-    this.img_alt = result.img_alt;
-    this.author = result.author;
-    this.date = result.date;
-    this.title = result.title;
-    this.description = result.description;
-    this.tags = result.tags;
+    // this.img_src = result.img_src;
+    // this.img_alt = result.img_alt;
+    // this.author = result.author;
+    // this.date = result.date;
+    // this.title = result.title;
+    // this.description = result.description;
+    // this.tags = result.tags;
 
     this.content = result.content.join('');
   }
